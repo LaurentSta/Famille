3ac2ebe5ccce6618745280418ed1e85c6d3511e5
Laurent Staelens <lostaelens@gmail.com>
2026-07-24 23:13:29 +0200

    Planning : min-h-0 sur la chaîne flex pour activer le défilement de la liste
    Les enfants flex ont un min-height:auto par défaut, ce qui empêchait
    overflow-y-auto de s'appliquer malgré le plafond de hauteur du cadre.

--- resources/views/livewire/meal-planner.blade.php
+++ resources/views/livewire/meal-planner.blade.php
@@ -29,8 +29,8 @@
                 </div>
             </template>
 
-            <div class="flex-1 flex flex-col bg-gradient-to-b from-brand-orange to-brand-red rounded-xl shadow-lg p-1 sticky top-4 z-10 max-h-[calc(100vh-2rem)]">
-                <div class="flex-1 flex flex-col bg-white rounded-lg p-2.5 sm:p-4">
+            <div class="flex-1 flex flex-col min-h-0 bg-gradient-to-b from-brand-orange to-brand-red rounded-xl shadow-lg p-1 sticky top-4 z-10 max-h-[calc(100vh-2rem)]">
+                <div class="flex-1 flex flex-col min-h-0 bg-white rounded-lg p-2.5 sm:p-4">
                     <input
                         type="text"
                         wire:model.live.debounce.300ms="search"
@@ -50,7 +50,7 @@
                         @endforeach
                     </div>
 
-                    <ul class="flex-1 space-y-1 overflow-y-auto">
+                    <ul class="flex-1 min-h-0 space-y-1 overflow-y-auto">
                         @foreach ($dishes as $dish)
                             @php
                                 $course = $dish->type === 'Dessert' ? 'dessert' : 'plat';
